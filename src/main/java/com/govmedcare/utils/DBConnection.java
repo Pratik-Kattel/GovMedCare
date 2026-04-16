181339c904cdd413882951abd947cec569b55009
Pratik-Kattel <kattelpratik007@gmail.com>
2026-04-17 05:30:31 +0545

    database connection completed

---
+++ src/main/java/com/govmedcare/utils/DBConnection.java
@@ -0,0 +1,30 @@
+package com.govmedcare.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection {
+    private static final Logger logger=Logger.getLogger(DBConnection.class.getName());
+    private static final String URL="jdbc:mysql://localhost:3306/govmedcare";
+    private static final String userName="root";
+    private static final String password="root";
+
+    static {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            logger.info("Driver loaded successfully");
+        }
+        catch (ClassNotFoundException e){
+            logger.log(Level.SEVERE,e.getMessage());
+        }
+    }
+    public static void getConnection() throws SQLException {
+        DriverManager.getConnection(URL, userName, password);
+    }
+    public static void main(String[] args) throws SQLException {
+        DBConnection.getConnection();
+    }
+}
