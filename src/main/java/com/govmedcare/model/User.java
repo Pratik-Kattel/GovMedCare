b96344637b799ed9f1f33cc8c381026ef508a8e4
Pratik-Kattel <kattelpratik007@gmail.com>
2026-04-17 03:03:20 +0545

    completed user model

---
+++ src/main/java/com/govmedcare/model/User.java
@@ -0,0 +1,100 @@
+package com.govmedcare.model;
+import com.govmedcare.types.UserRole;
+import com.govmedcare.types.UserStatus;
+import java.sql.Timestamp;
+
+
+public class User {
+    private long id;
+    private String name;
+    private String email;
+    private  String password;
+    private String roles;
+    private UserRole role;
+    private Long phone;
+    private String address;
+    private UserStatus status;
+    private Timestamp created_at;
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+
+}
--- src/main/java/com/govmedcare/model/UserModel.java
+++
@@ -1,4 +0,0 @@
-package com.govmedcare.model;
-
-public class UserModel {
-}
