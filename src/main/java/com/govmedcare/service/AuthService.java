1c42cfe2030ea38b359e971d3b24a2de25acbaf6
Pratik-Kattel <kattelpratik007@gmail.com>
2026-04-21 23:16:56 +0545

    added block account logic

--- src/main/java/com/govmedcare/service/AuthService.java
+++ src/main/java/com/govmedcare/service/AuthService.java
@@ -2,6 +2,7 @@ package com.govmedcare.service;
 import com.govmedcare.dao.UserDao;
 import com.govmedcare.exception.InvalidCredentialsException;
 import com.govmedcare.exception.UserAlreadyExistsException;
+import com.govmedcare.exception.UserBlockedException;
 import com.govmedcare.exception.UserDoesNotExistsException;
 import com.govmedcare.model.User;
 import com.govmedcare.types.UserRole;
@@ -23,6 +24,9 @@ public class AuthService {
     }
     public User LoginUserService(String email,String password){
         User existingUser=userDao.findByEmail(email).orElseThrow(()->new UserDoesNotExistsException("User with this email doesn't exists please register to continue"));
+        if(existingUser.getStatus()==UserStatus.blocked){
+            throw new UserBlockedException("Account blocked, please contact administration for support");
+        }
         boolean validPassword=BCrypt.checkpw(password,existingUser.getPassword());
         if(!validPassword){
             throw new InvalidCredentialsException("Invalid Credentials provided");
