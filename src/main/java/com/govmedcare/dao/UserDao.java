6c4bd39dab5d884f40149135e91743d6c5ecb2af
Pratik-Kattel <kattelpratik007@gmail.com>
2026-05-03 14:44:13 +0545

    Added admin block feature

--- src/main/java/com/govmedcare/dao/UserDao.java
+++ src/main/java/com/govmedcare/dao/UserDao.java
@@ -1,14 +1,18 @@
 package com.govmedcare.dao;
+
 import com.govmedcare.model.User;
 import com.govmedcare.repository.UserRepository;
 import com.govmedcare.types.UserRole;
 import com.govmedcare.types.UserStatus;
 import com.govmedcare.utils.DBConnection;
 import com.govmedcare.utils.QueryUtil;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -56,9 +60,9 @@ public class UserDao implements UserRepository {
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(QueryUtil.checkByEmail)
         ) {
-            ps.setString(1,email);
-            ResultSet rs=ps.executeQuery();
-            if(rs.next()) {
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
                 User user = new User(
                         rs.getLong("user_id"),
                         rs.getString("name"),
@@ -74,9 +78,45 @@ public class UserDao implements UserRepository {
             }
 
         } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Query not executed",e.getMessage());
+            logger.log(Level.SEVERE, "Query not executed", e.getMessage());
         }
         return Optional.empty();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> list = new ArrayList<>();
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(QueryUtil.getUsers)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong("user_id"));
+                user.setName(rs.getString("name"));
+                user.setAddress(rs.getString("Address"));
+                user.setPhone(rs.getString("phone"));
+                list.add(user);
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Failed to get users", e.getMessage());
+        }
+        return list;
+    }
+
+    @Override
+    public boolean blockUser(Long id) {
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(QueryUtil.blockUser)
+        ) {
+            int rows = ps.executeUpdate();
+            if (rows > 0) return true;
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Failed to block User", e.getMessage());
+
+        }
+        return false;
     }
 }
 
