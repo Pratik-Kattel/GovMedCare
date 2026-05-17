72f393d04d27f8f6413e323448a03a5d77f11771
Pratik-Kattel <kattelpratik007@gmail.com>
2026-05-17 08:42:04 +0545

    Completed financial report generating feature

---
+++ src/main/java/com/govmedcare/dao/ReportDao.java
@@ -0,0 +1,32 @@
+package com.govmedcare.dao;
+
+import com.govmedcare.repository.ReportRepository;
+import com.govmedcare.utils.DBConnection;
+import com.govmedcare.utils.QueryUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ReportDao implements ReportRepository {
+    Logger logger = Logger.getLogger(ReportRepository.class.getName());
+
+    @Override
+    public double generateReport() {
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(QueryUtil.calculateTotalRevenue)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getDouble("total_revenue");
+            }
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Failed to fetch the total revenue");
+
+        }
+        return 0;
+    }
+}
